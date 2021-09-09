857f15f3422c99bf103ca6b0c1fb767a822361ac
KroryBloody <Josegregoriohndz1512@gmail.com>
2021-09-09 22:55:38 +0200

    aaa

--- archivos/json/listas.js
+++ archivos/json/listas.js
@@ -12,5 +12,13 @@ var elm={
             desc:" Permite la impresion en metales con bajo punto de fusion, como aluminio, cobre, plata, zinc...etc, ademas de aleaciones de los mismos",
             req:"Horno de fundido para 1000º, Extrusor para metales, Analizador de aleaciones"
         } 
+    ],
+    recursos:[
+        {
+        Reservas:"a",
+        Descripcion:"e",
+        Tipo:"i"
+        }
+        
     ]
 }
